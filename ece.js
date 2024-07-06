4025241ddf7266a8af607f378db10ac6e51a4160
zeynepecekiris <zeynep.ece933@gmail.com>
2024-07-06 18:51:52 +0300

    first commit

---
+++ ece.js
@@ -0,0 +1,20 @@
+const not = 'AA';
+switch(not) {
+    case 'AA':
+        console.log("En basarili Sensin");
+        break;
+    case 'BA':
+        console.log("Super not");
+        break;
+    case 'BB':
+        console.log("guzel not");
+        break;
+    case 'CB':
+        console.log("Idare eder");
+        break;
+    case 'CC':
+        console.log("hadi yine iyisin");
+        break;
+    default:
+        console.log("hobaaaa");
+}
